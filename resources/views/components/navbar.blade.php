19dd1d808fa1215acd903fe8bac9c60bedec1ea3
Luth-Badila <FrancescoLutfi10@gmail.com>
2024-06-09 19:15:11 +0700

    add: added active link in navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse d-none d-lg-block" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <x-nav-link href="/" :active="request() -> is('/')">Home</x-nav-link>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/about">About</a>
